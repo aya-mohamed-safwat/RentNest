8e22dee0e2ef461916e6870c130cbc6bc27eac9d
Aya Mohamed Safwat <ayamohamedsafwat1@gmail.com>
2024-03-31 19:36:02 +0200

    edit house

--- src/main/java/com/example/RentNest/houses/HouseMapper.java
+++ src/main/java/com/example/RentNest/houses/HouseMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
+
 import java.util.List;
 
 @Mapper(componentModel = "spring")
